8f23e302b7e7e55586a9912e4da953860642b2f2
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-05-11 21:46:38 +0200

    username control

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,31 @@ function App() {
   });
   const [isErrorSelect, setIsErrorSelect] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [isUsername, setIsUsername] = useState(false);
+  const [isUsernameLength, setIsUsernameLength] = useState(false);
 
   const handleInputs = (e) => {
     const { name, value } = e.target;
     console.log(value);
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = `!@#$%^&*()-_=+[]{}|;:'\\£",.<>?/~`;
+    const lastLetter = value.slice(-1);
+    console.log(lastLetter);
+    if (letters.includes(lastLetter)) {
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+      setIsUsername(false);
+    } else {
+      setIsUsername(true);
+      return;
+    }
   };
   const handleForm = (e) => {
     e.preventDefault();
+
     if (inputs.select === "") {
       setIsErrorSelect(true);
     } else {
@@ -38,14 +52,20 @@ function App() {
     ) {
       setIsError(true);
       setIsErrorSelect(false);
+      setIsUsername(false);
+      setIsUsernameLength(false);
     } else {
       setIsError(false);
+    }
+    console.log();
+    if (inputs.username.length < 6) {
+      setIsUsernameLength(true);
     }
   };
   return (
     <>
       <form action="" className="formSign" onSubmit={handleForm}>
-        <div>
+        <div className="inputContainer">
           <label className="labelForm"> Real name:</label>
           <input
             type="text"
@@ -55,7 +75,7 @@ function App() {
             className="inputs"
           />
         </div>
-        <div>
+        <div className="inputContainer">
           <label className="labelForm"> Username:</label>
           <input
             type="text"
@@ -64,8 +84,26 @@ function App() {
             onChange={handleInputs}
             className="inputs"
           />
+          <p style={{ marginTop: 0 }}>
+            Deve contenere solo caratteri alfanumerici e almeno 6 caratteri (no
+            spazi o simboli).
+          </p>
+          {isUsername && (
+            <div className="ErrorInput">
+              <div>
+                <p>Metti un Carattere valido!</p>
+              </div>
+            </div>
+          )}
+          {isUsernameLength && (
+            <div className="ErrorInput">
+              <div>
+                <p>Input Username almeno 6 caratteri!</p>
+              </div>
+            </div>
+          )}
         </div>
-        <div>
+        <div className="inputContainer">
           <label className="labelForm"> Password:</label>
 
           <input
@@ -90,7 +128,7 @@ function App() {
             <option value="Backend">Backend</option>
           </select>
         </div>
-        <div>
+        <div className="inputContainer">
           <label className="labelForm"> Anni di esperienza:</label>
 
           <input
