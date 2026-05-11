8aad1e503fc9a18b184e963a8c5057e1e2700bf7
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-05-11 23:45:47 +0200

    crazy control

--- src/App.jsx
+++ src/App.jsx
@@ -11,39 +11,90 @@ function App() {
     description: "",
     select: "",
   });
+
+  //
+  // Keyboard
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = `!@#$%^&*()-_=+[]{}|;:'\\£",.<>?/~`;
+
+  //
+  // ERROR STATES
+  // Select Error
   const [isErrorSelect, setIsErrorSelect] = useState(false);
+
+  // General Error Empty
   const [isError, setIsError] = useState(false);
+
+  // Username Check
   const [isUsername, setIsUsername] = useState(false);
   const [isUsernameLength, setIsUsernameLength] = useState(false);
 
+  // Password Check
+  const [isPasswordLength, setIsPasswordLength] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+
   const handleInputs = (e) => {
     const { name, value } = e.target;
-    console.log(value);
-    const letters = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const symbols = `!@#$%^&*()-_=+[]{}|;:'\\£",.<>?/~`;
-    const lastLetter = value.slice(-1);
-    console.log(lastLetter);
-    if (letters.includes(lastLetter)) {
-      setInputs({
-        ...inputs,
-        [name]: value,
-      });
-      setIsUsername(false);
-    } else {
-      setIsUsername(true);
-      return;
-    }
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
   };
+
+  //
+  //
   const handleForm = (e) => {
     e.preventDefault();
 
-    if (inputs.select === "") {
-      setIsErrorSelect(true);
-    } else {
-      setIsErrorSelect(false);
-      console.log(inputs);
+    const password = inputs.password;
+    const username = inputs.username;
+    const falseandtrue = [];
+
+    for (let index = 0; index < username.length; index++) {
+      const element = username[index];
+      const contain = symbols.includes(element);
+      const containNumber = numbers.includes(element);
+
+      falseandtrue.push(contain);
+      falseandtrue.push(containNumber);
+      if (falseandtrue.find((el) => el === true)) {
+        setIsUsername(true);
+        return;
+      }
     }
+
+    //
+    //
+
+    const passwordNumber = [];
+    const passwordSymbol = [];
+    const passwordLetter = [];
+
+    for (let index = 0; index < password.length; index++) {
+      const element = password[index];
+      const containNumber = numbers.includes(element);
+      const containSymbol = symbols.includes(element);
+      const containLetter = letters.includes(element);
+
+      passwordSymbol.push(containSymbol);
+      passwordNumber.push(containNumber);
+      passwordLetter.push(containLetter);
+
+      if (
+        passwordNumber.find((el) => el === true) &&
+        passwordSymbol.find((el) => el === true) &&
+        passwordLetter.find((el) => el === true)
+      ) {
+        setIsPassword(false);
+      } else {
+        setIsPassword(true);
+      }
+    }
+    //
+    //
+
     if (
       inputs.name === "" ||
       inputs.description === "" ||
@@ -54,17 +105,48 @@ function App() {
       setIsErrorSelect(false);
       setIsUsername(false);
       setIsUsernameLength(false);
+      setIsPassword(false);
+      setIsPasswordLength(false);
+      return;
     } else {
       setIsError(false);
     }
-    console.log();
+
+    // Password validation and username validation for length
+    if (inputs.password.length < 8 && inputs.username.length < 6) {
+      setIsPasswordLength(true);
+      setIsUsernameLength(true);
+
+      return;
+    } else {
+      setIsPasswordLength(false);
+      setIsUsernameLength(false);
+    }
     if (inputs.username.length < 6) {
       setIsUsernameLength(true);
+      return;
+    } else {
+      setIsUsernameLength(false);
     }
+
+    if (inputs.password.length < 8) {
+      setIsPasswordLength(true);
+      return;
+    } else {
+      setIsPasswordLength(false);
+    }
+
+    //
+    //
+    // if () {
+    // }
+    console.log(inputs);
   };
   return (
     <>
       <form action="" className="formSign" onSubmit={handleForm}>
+        {/* REAL NAME */}
+
         <div className="inputContainer">
           <label className="labelForm"> Real name:</label>
           <input
@@ -75,6 +157,9 @@ function App() {
             className="inputs"
           />
         </div>
+
+        {/* USERNAME */}
+
         <div className="inputContainer">
           <label className="labelForm"> Username:</label>
           <input
@@ -88,6 +173,8 @@ function App() {
             Deve contenere solo caratteri alfanumerici e almeno 6 caratteri (no
             spazi o simboli).
           </p>
+
+          {/* Username Validation for symbols */}
           {isUsername && (
             <div className="ErrorInput">
               <div>
@@ -95,6 +182,8 @@ function App() {
               </div>
             </div>
           )}
+
+          {/* Username Validation For length */}
           {isUsernameLength && (
             <div className="ErrorInput">
               <div>
@@ -103,6 +192,10 @@ function App() {
             </div>
           )}
         </div>
+
+        {/*  */}
+
+        {/* PASSWORD */}
         <div className="inputContainer">
           <label className="labelForm"> Password:</label>
 
@@ -113,7 +206,29 @@ function App() {
             onChange={handleInputs}
             className="inputs"
           />
+          <p style={{ marginTop: 0 }}>
+            Deve contenere almeno 8 caratteri, 1 lettera, 1 numero e 1 simbolo e
+            una lettera
+          </p>
+          {/* Username Validation For length */}
+          {isPasswordLength && (
+            <div className="ErrorInput">
+              <div>
+                <p>Input Password almeno 8 caratteri!</p>
+              </div>
+            </div>
+          )}
+          {isPassword && (
+            <div className="ErrorInput">
+              <div>
+                <p>La password deve almeno contenere 1 simbolo e 1 numero</p>
+              </div>
+            </div>
+          )}
         </div>
+
+        {/* RUOLO */}
+
         <div className="selectForm">
           <label className="labelForm"> Ruolo:</label>
           <select
@@ -128,6 +243,8 @@ function App() {
             <option value="Backend">Backend</option>
           </select>
         </div>
+
+        {/* YEAR */}
         <div className="inputContainer">
           <label className="labelForm"> Anni di esperienza:</label>
 
@@ -140,6 +257,9 @@ function App() {
             min={0}
           />
         </div>
+
+        {/* DESCRIPTION */}
+
         <div className="textareaContainer">
           <label className="labelForm"> Descrizione:</label>
 
@@ -153,6 +273,8 @@ function App() {
         </div>
         <button>Registrati!</button>
       </form>
+
+      {/* ERROR FOR SELECT */}
       {isErrorSelect && (
         <div className="ErrorInput">
           <div>
